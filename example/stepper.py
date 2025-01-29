0a9b86f9b7e697fd96d9121a05838386a5473c53
Ninth2234 <85066094+Ninth2234@users.noreply.github.com>
2025-01-29 16:09:17 +0700

    update stepper example

--- example/stepper.py
+++ example/stepper.py
@@ -1,32 +1,21 @@
 from machine import Pin
 from time import sleep
 
+stp = Pin(0,Pin.OUT)
+dir = Pin(1,Pin.OUT)
 
-
-pin0 = Pin(0,Pin.OUT)
-pin1 = Pin(1,Pin.OUT)
-pin2 = Pin(2,Pin.OUT)
-pin3 = Pin(3,Pin.OUT)
-
-pause_time = 0.002
+delay_time = 0.02
+step = 100
 while True:
-    for i in range(500):
-        pin0.high()
-        pin2.high()
-        sleep(pause_time)
-
-        pin0.low()
-        pin2.low()
-        sleep(pause_time)
-
-    pin1.high()
-    pin3.high()
-
-    for i in range(500):
-        pin0.high()
-        pin2.high()
-        sleep(pause_time)
-
-        pin0.low()
-        pin2.low()
-        sleep(pause_time)
+    dir.on()
+    for i in range(step):
+        stp.on()
+        sleep(delay_time/2)
+        stp.off()
+        sleep(delay_time/2)
+    dir.off()
+    for i in range(step):
+        stp.on()
+        sleep(delay_time/2)
+        stp.off()
+        sleep(delay_time/2)        
